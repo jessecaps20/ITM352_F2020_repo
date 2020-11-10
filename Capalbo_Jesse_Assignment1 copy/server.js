3aa5d9a66614eda09ea4499d8a78921ebe8ee3f8
Jesse Capalbo <47260861+jessecaps20@users.noreply.github.com>
2020-11-09 14:09:04 -1000

    added assignment 1 copy/ comments

--- Capalbo_Jesse_Assignment1 copy/server.js
+++ Capalbo_Jesse_Assignment1 copy/server.js
@@ -1,7 +1,7 @@
 
 // Java Script Server 
-// Referenced from Assignment 1 screencast and Lab13
-var products = require("./public/product_data.json");
+// copied/referenced from Assignment 1 screencast and Lab13 and stack exchange
+var products = require("./public/product_data.json");//load products from json file
 var querystring = require('querystring'); //server responds to any errors
 var express = require('express'); //express package
 var app = express(); // initialize express
@@ -12,14 +12,12 @@ app.all('*', function (request, response, next) { //for all request methods
     next(); 
 });
 app.use(myParser.urlencoded({ extended: true })); //get data in the body
-
 app.post("/process_form", function (request, response) { //validate quantity data before sending to invoice
     let POST = request.body; 
     if (typeof POST['submitPurchase'] != 'undefined') {
         response.send("Invalid Entry!");
     }
-
-    let validQuantities = [];
+    let validQuantities = []; //adding to an array to check all quantities
     let hasErrors = false; 
     for (i in products) {
         qty = POST[`quantity${i}`];
@@ -27,12 +25,10 @@ app.post("/process_form", function (request, response) { //validate quantity dat
     }
     hasErrors = validQuantities.includes(false);
     const stringified = querystring.stringify(POST);// if all quantities are valid invoice is generated
-
     if (validQuantities.length != 0 && !hasErrors) {
         response.redirect("./invoice.html?" + stringified); 
     }
     else { response.send('Enter a valid quantity!') } // if quantities are not valid send message
-    
 });
 function isNonNegInt(q, returnErrors = false) { //isNonNegInt function that returns errors 
     errors = []; 
