f1cf98e942e0bd53223aa13f837829278ce1e07d
Jesse Capalbo <47260861+jessecaps20@users.noreply.github.com>
2020-11-10 18:25:17 -1000

    hello Jesse! from, Jesse

--- Capalbo_Jesse_Assignment1/server.js
+++ Capalbo_Jesse_Assignment1/server.js
@@ -9,17 +9,17 @@ var myParser = require("body-parser"); //load body parser
 
 app.all('*', function (request, response, next) { //for all request methods
     console.log(request.method + ' to ' + request.path); //type in the console the request method and the path
-    next(); 
+    next(); // program can move on
 });
 app.use(myParser.urlencoded({ extended: true })); //get data in the body
 app.post("/process_form", function (request, response) { //validate quantity data before sending to invoice
-    let POST = request.body; 
+    let POST = request.body; // packages data in the body
     if (typeof POST['submitPurchase'] != 'undefined') {
         response.send("Invalid Entry!");
     }
-    let validQuantities = []; //adding to an array to check all quantities
-    let hasErrors = false; 
-    for (i in products) {
+    let validQuantities = []; //array assuiming quantities are valid all quantities
+    let hasErrors = false; // develop a variable to assume that is false
+    for (i in products) {  //returns true if the "i" is in the specified object of products
         qty = POST[`quantity${i}`];
         if (qty != '' || qty > 0) validQuantities.push(isNonNegInt(qty)); // if both a quantity over 0 and valid
     }
@@ -31,11 +31,11 @@ app.post("/process_form", function (request, response) { //validate quantity dat
     else { response.send('Enter a valid quantity!') } // if quantities are not valid send message
 });
 function isNonNegInt(q, returnErrors = false) { //isNonNegInt function that returns errors 
-    errors = []; 
+    errors = []; // assume that quantity data is valid 
     if (q == "") { q = 0; }
-    if (Number(q) != q) errors.push('Not a number!'); 
-    if (q < 0) errors.push('Negative value!'); 
-    if (parseInt(q) != q) errors.push('Not an integer!'); 
+    if (Number(q) != q) errors.push('Not a number!'); //check if the string is a number
+    if (q < 0) errors.push('Negative value!'); //check if value is a positive
+    if (parseInt(q) != q) errors.push('Not an integer!'); //check if value is an integer
     return returnErrors ? errors : (errors.length == 0);
 }
 app.use(express.static('./public')); //set up a static route with public folder
