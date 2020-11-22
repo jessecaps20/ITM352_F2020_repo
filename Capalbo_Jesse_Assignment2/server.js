33ed869ff99a16bf6c04a4708b7d09ca3fc416ca
Jesse Capalbo <47260861+jessecaps20@users.noreply.github.com>
2020-11-22 00:53:34 -1000

    Big Commit, lots of changes to assignment 2

--- Capalbo_Jesse_Assignment2/server.js
+++ Capalbo_Jesse_Assignment2/server.js
@@ -1,8 +1,8 @@
 
 // Java Script Server 
-// copied/referenced from Assignment 1 screencast and Lab13 and stack exchange
+// copied/referenced from Assignment 1 screencast, Lab13, Lab14 and stack exchange
 var products = require("./public/product_data.json");//load products from json file
-const queryString = require('queryString'); //server responds to any errors
+const querystring = require('querystring'); //server responds to any errors
 var express = require('express'); //express package
 var app = express(); // initialize express
 var myParser = require("body-parser"); //load body parser 
@@ -16,11 +16,14 @@ if (fs.existsSync(filename)) { //Only open the file if it exists
 } else {
     console.log(filename + 'does not exist!'); //filename does not exist
 }
+
 app.all('*', function (request, response, next) { //for all request methods
     console.log(request.method + ' to ' + request.path); //type in the console the request method and the path
     next(); // program can move on
 });
+
 app.use(myParser.urlencoded({ extended: true })); //get data in the body
+
 app.post("/process_form", function (request, response) { //validate quantity data before sending to invoice
     let POST = request.body; // packages data in the body
     console.log(POST)
@@ -31,15 +34,16 @@ app.post("/process_form", function (request, response) { //validate quantity dat
     let hasErrors = false; // develop a variable to assume that is false
     for (i in products) {  //returns true if the "i" is in the specified object of products
         qty = POST[`quantity${i}`];
-        console.log(i, qty)
+        ///console.log(i, qty)
         if (qty != '' || qty > 0) validQuantities.push(isNonNegInt(qty)); // if both a quantity over 0 and valid
     }
     hasErrors = validQuantities.includes(false);
-    const stringified = queryString.stringify(POST);// if all quantities are valid invoice is generated
+    const stringified = querystring.stringify(POST);// if all quantities are valid invoice is generated
     if (validQuantities.length != 0 && !hasErrors) {
         response.redirect("./login.html?" + stringified);
     }  else { response.redirect("./product_display.html?" + stringified) } // if quantities are not valid send message
 });
+
 //From Lab 13
 //Server-side processing
 app.post("/process_login", function (request, response) {
@@ -47,15 +51,14 @@ app.post("/process_login", function (request, response) {
     console.log(request.query);
     the_username = request.body.username.toLowerCase(); //makes username case insensitive
     if (typeof users_reg_data[the_username] != 'undefined') { //check if username actually exists
-        if (users_reg_data[the_username].password == request.body.password) {
+        if (users_reg_data[the_username].password == request.body.password) { //if the username exists ask for the password
             request.query.username = the_username;
             console.log(users_reg_data[request.query.username].name);
             request.query.name = users_reg_data[request.query.username].name;
-            response.redirect('/invoice.html?' + queryString.stringify(request.query));
-            return;
-            //if all good redirect to invoice//
+            response.redirect('/invoice.html?' + querystring.stringify(request.query));//if all good redirect to invoice
+            return; //if all good redirect to invoice//
         } else {
-            LogError.push = ('Invalid Password'); //Password does not exist/is wrong
+            LogError.push = ('Invalid Password'); //Password does not exist or is wrong
             console.log(LogError);
             request.query.username = the_username;
             request.query.name = users_reg_data[the_username].name;
@@ -67,20 +70,19 @@ app.post("/process_login", function (request, response) {
         request.query.username = the_username;
         request.query.LogError = LogError.join(';');
     }
-    response.redirect('./login.html?' + queryString.stringify(request.query));
+    response.redirect('./login.html?' + querystring.stringify(request.query));
 });
 //From Lab 13
 //Server-side processing
 app.post("/process_register", function (request, response) {
-    qstr = request.body;
-    console.log(qstr);
+    console.log(request.body);
     var errors = [];
     var reguser = request.body.username.toLowerCase(); //makes username case insensitive
     if (typeof users_reg_data[reguser] != 'undefined') { //Check if username already exists
         errors.push('Username is Already in Use.')
     }
-    if ((/[a-z0-9]+/).test(request.body.username) == false) {
-        errors.push('Numbers and Letters only'); //Check if there are other symbols
+    if ((/[a-z0-9]+/).test(request.body.username) == false) {//Check if there are other symbols instead of numberd and letters
+        errors.push('Numbers and Letters only'); //Check if there are other symbols instead of numberd and letters
     }
     if ((request.body.password.length < 6 == true)) {//password validation
         errors.push('Password Too Short - 6 characters minimum'); //Check if password is too short
@@ -88,15 +90,14 @@ app.post("/process_register", function (request, response) {
     if (request.body.password !== request.body.repeat_password) {// check to see if passwords match
         errors.push('Passwords Do Not Match') //Check if passwords match
     }
-    if (errors.length == 0) {
+    if (errors.length == 0) {   //if no errors remember user's registration in json made 
         console.log('none');
         request.query.username = reguser; //Input username into JSON file array
-        //request.query.name = request.body.name; //Input fullname in array
         request.query.password = request.body.password; //Input password in array
         fs.writeFileSync(filename, JSON.stringify(users_reg_data)); //Write it out in the JSON file to store data
-        response.redirect('/invoice.html?' + queryString.stringify(request.query)); //send to invoice if all good
+        response.redirect('/invoice.html?' + querystring.stringify(request.query)); //send to invoice if all good
     }
-    if (errors.length > 0) {
+    if (errors.length > 0) { //directs user to registration page if there are any errrors
         console.log(errors)
         request.query.name = request.body.name;
         request.query.username = request.body.username;
@@ -104,9 +105,10 @@ app.post("/process_register", function (request, response) {
         request.query.repeat_password = request.body.repeat_password;
         request.query.email = request.body.email;
         request.query.errors = errors.join(';');
-        response.redirect('registration.html?' + queryString.stringify(request.query));
+        response.redirect('registration.html?' + querystring.stringify(request.query));
     }
 });
+
 function isNonNegInt(q, returnErrors = false) { //isNonNegInt function that returns errors 
     errors = []; // assume that quantity data is valid 
     if (q == "") { q = 0; }
@@ -115,5 +117,6 @@ function isNonNegInt(q, returnErrors = false) { //isNonNegInt function that retu
     if (parseInt(q) != q) errors.push('Not an integer!'); //check if value is an integer
     return returnErrors ? errors : (errors.length == 0);
 }
+
 app.use(express.static('./public')); //set up a static route with public folder
 app.listen(8080, () => console.log(`listening on port 8080`)); //run server on port 8080
